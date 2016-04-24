934343f2bfcf25de1ac6c3d1ef57a1d031a96a54
ann73 <ann73@DESKTOP-RR3R29H>
2016-04-24 21:46:17 +0900

    다시 받은거 수정

---
+++ shopERP/WebContent/js/main.js
@@ -0,0 +1,14 @@
+
+//팝업창 클릭이벤트
+$(function(){
+	$('#newAdd').click(function(){
+		$('.newAddCard').css("display","block")
+	})
+	$('#closeBtn').click(function(){
+		$('.newAddCard').css("display","none")
+	})
+	
+	$("#btn-footer-Close").click(function() {
+		$('.newAddCard').css("display","none")
+	})
+});
